30287c46b2f5df2ba943f78fcd579e8beb6c771f
katehrsn <kate.hrsn@gmail.com>
2021-04-22 20:48:51 +0100

    DOM refactored

--- main.js
+++ main.js
@@ -1,93 +1,127 @@
-let play = true;
+// DOM Variables
+const newGameButton = document.querySelector("#newGameButton");
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+const userScore = document.querySelector("#userScore");
+const computerScore = document.querySelector("#computerScore");
+const tieGames = document.querySelector("#tieGames");
 
-let score = 0
+// Game Variables
+let computerMove = "n/a";
+let playerMove = "n/a";
 
-let gamesPlayed = 0
+let playerWins = 0;
+let computerWins = 0;
+let ties = 0;
 
-let gamesLost = 0
-let gamesDraw = 0
-let gamesWon = 0
+let result = "n/a"
 
-while(play === true) {
+// Event Listeners
+newGameButton.addEventListener("click", newGame);
+rockButton.addEventListener("click", userRock);
+paperButton.addEventListener("click", userPaper);
+scissorsButton.addEventListener("click", userScissors);
 
-let randomNumber = Math.floor((Math.random() * 3) + 1);
+// Functions
+function newGame(){
+    playerWins = 0
+    computerWins = 0
+    ties = 0
+    playerMove = "n/a"
+    computerMove = "n/a"
+    result = "n/a"
+    userScore.innerText = `user: ${playerWins}`
+    computerScore.innerText = `computer: ${computerWins}`
+    tieGames.innerText = `ties: ${ties}`
 
-function computerMove() {
-    if (randomNumber === 1) {
-        return "rock" ; 
-    }
-    else if (randomNumber === 2) {
-        return "paper" ;
-    }
-    else if (randomNumber === 3) {
-        return "scissors" ;
-    }
 }
 
-console.log(computerMove());
-
-let userInput = prompt("rock, paper, or scissors?");
+function randomNumber(){
+    return Math.floor((Math.random() * 3) + 1);
+}
+function getComputerMove(){
+    randomNumber();
+    if (randomNumber() === 1) {
+        return computerMove = "rock";
+    }
+    else if (randomNumber() === 2) {
+        return computerMove = "paper";
+    }
+    else if(randomNumber() === 3) {
+        return computerMove = "scissors";
+    }
+}
 
 function getWinner (player1, player2) {
  if (player1 === "rock") {
     if (player2 === "rock") {
-        return 0 ;
+        return "tie" ;
     }
     else if (player2 === "paper") {
-        return -1 ;
+        return "computer wins" ;
     }
     else if (player2 === "scissors") {
-        return 1 ;
+        return "user wins" ;
     }
  }
  else if (player1 === "paper") {
     if (player2 === "rock") {
-        return 1 ;
+        return "user wins" ;
     }
     else if (player2 === "paper") {
-        return 0 ;
+        return "tie" ;
     }
     else if (player2 === "scissors") {
-        return -1 ;
+        return "computer wins" ;
     }
  }
  else if (player1 === "scissors") {
     if (player2 === "rock") {
-        return -1 ;
+        return "computer wins" ;
     }  
     else if (player2 === "paper") {
-        return 1 ;
+        return "user wins" ;
     }
     else if (player2 === "scissors") {
-        return 0 ;
+        return "tie" ;
     }
  }
 }
 
-let result = getWinner(userInput, computerMove());
-
-score = score + result
-
-gamesPlayed = gamesPlayed + 1
-
-if (result === 1) {
-    gamesWon = gamesWon + 1
-}
-else if (result === 0) {
-    gamesDraw = gamesDraw + 1
-}
-else if (result === -1) {
-    gamesLost = gamesLost + 1
-}
-
-
-alert(`your score is ${score}, you have played ${gamesPlayed} games, you have won ${gamesWon} games, draw ${gamesDraw} games, and lost ${gamesLost} games`)
-
-let playAgain = confirm("Do you want to play again?");
-    if (playAgain === true) {
-        play = true;
+function getScore() {
+    if (result === "user wins"){
+        playerWins++;
+        userScore.innerText = `user: ${playerWins}`
     }
-    else {
-        play = false;
+    else if (result === "computer wins"){
+        computerWins++;
+        computerScore.innerText = `computer: ${computerWins}`
     }
+    else if (result === "tie"){
+        ties++;
+        tieGames.innerText = `ties: ${ties}`
+    }
+}
+
+function userRock(){
+    let computerMove = getComputerMove();
+    let playerMove = "rock";
+    let winner = getWinner(playerMove, computerMove)
+    result = winner
+    getScore();
+}
+function userPaper(){
+    let computerMove = getComputerMove();
+    let playerMove = "paper";
+    let winner = getWinner(playerMove, computerMove)
+    result = winner
+    getScore();
+}
+function userScissors(){
+    let computerMove = getComputerMove();
+    let playerMove = "scissors";
+    let winner = getWinner(playerMove, computerMove)
+    result = winner
+    getScore();
 }
